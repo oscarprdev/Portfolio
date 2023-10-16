587c623d0fffb821a5dfdb18f1c3b382843394f0
oscarprdev <vlcoscar8@gmail.com>
2023-10-16 11:58:08 +0200

    feat: update vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import vercelServerless from '@astrojs/vercel/serverless';
 
 import react from '@astrojs/react';
 
@@ -8,5 +8,6 @@ import react from '@astrojs/react';
 export default defineConfig({
     integrations: [tailwind(), react()],
     output: 'server',
-    adapter: vercel()
+    adapter: vercelServerless(),
+    server: { port: 1234, host: true }
 });
