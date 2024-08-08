17975ed1f33afc16e99720a1726b3de64fe9e685
oscarprdev <vlcoscar8@gmail.com>
2024-08-08 11:34:19 +0200

    Add project scafolding

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
+
+// https://astro.build/config
+export default defineConfig({
+    i18n: {
+        defaultLocale: 'es',
+        locales: ['es', 'en'],
+        routing: {
+            prefixDefaultLocale: false
+        }
+    },
+    output: 'server',
+    adapter: vercel()
+});
